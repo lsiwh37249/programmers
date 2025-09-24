3853dc4b83b896fd0c865ff5e4818f7a9cb04b09
samdul-t4-tl-KimRyeongRae <108792838+lsiwh37249@users.noreply.github.com>
2025-09-25 06:56:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.89 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,14 +1,10 @@
 def solution(s):
-    answer = []
     depth = 0
     z_count = 0
-    while True:
-        before_s = len(s)
-        after_s = len(s.replace("0",""))
-        z_count = z_count + (before_s - after_s)
-        depth = depth + 1
-        s = bin(after_s)[2:]
-        if s == "1":
-            break
     
+    while s != "1":
+        z_count += s.count("0")
+        s = bin(len(s.replace("0", "")))[2:]
+        depth += 1
+
     return [depth, z_count]
